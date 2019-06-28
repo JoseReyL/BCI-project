d5f0db527b53d071e0ca301a090ca1091139dea4
LoreVala <43637048+LoreVala@users.noreply.github.com>
2019-06-28 15:05:26 +0200

    code refining

--- TrainingSignals_ErrPs.py
+++ TrainingSignals_ErrPs.py
@@ -3,7 +3,6 @@ import numpy as np
 import pickle, glob
 import sys,os
 from scipy.fftpack import fft
-#import linear
 import sklearn
 import sklearn.linear_model
 from sklearn import preprocessing
@@ -20,35 +19,41 @@ sigProcPath = os.path.join(os.path.abspath(pydir),'../../python/signalProc')
 sys.path.append(sigProcPath)
 import preproc
 
+### Variables ###
+data_folder = 'error_potentials'    # folder containing training data
+spatialfilter='car' #type of spatial filter
+freqbands    = [8, 10, 28, 30]  # frequency bands for the spectral filter
+cname_time = 'ErrP_clf_time'    # name of the time classifier
+treshold_time = 0.4431697721955611 # optimal threshold for the time classifier (precomputed)
+cname_freq = 'ErrP_clf_freq'    # name for the frequency classifier
+treshold_freq = 0.45619815088221327 # optimal threshold for the frequency classifier (precomputed)
 
-dataPath = os.path.join(os.path.abspath(pydir),'error_potentials/training_data_ErrPs-')
+
+# grouping all the ErrPs data files
+dataPath = os.path.join(os.path.abspath(pydir),data_folder)
 exp = []
 exp_name = []
-for i in range(1,15):
-    file = '/error_potentials/training_data_ErrPs-'
-    if i == 4: continue
-    print(i, dataPath+str(i)+'.pk')
-    f = open(dataPath+str(i)+'.pk', 'rb')
-    exp_name.append(file+str(i))
+for file in os.listdir(dataPath):
+    if file == '.ipynb_checkpoints': continue # exception for non data file
+    print(file)
+    f = open(os.path.join(dataPath,file), 'rb')
+    exp_name.append(file)
     exp.append(pickle.load(f))
-
+    
+# for discriminate correct Errp from incorrect 
 bad = []
 good = []
 
-
-
-#Counters
-
+# counters
 bad_count = 0
 good_count = 0
-both = 0
-none = 0
 
+# Data preprocessing 
 for n_exp in range(len(exp)):
     print(' ***********   Experiment  '+exp_name[n_exp]+'   *********** ')
     data           =   np.array(exp[n_exp]['data'])   #data support variable
     data           =   np.transpose(data)
-    # Take only 1s of EEG recording (We originally took 2s but 1s is enough)
+    # Take only the first 1s of EEG recording (We originally took 2s but 1s is enough)
     data = data[:,0:250,:]
     # 1: detrend
     data        = preproc.detrend(data)
@@ -56,10 +61,8 @@ for n_exp in range(len(exp)):
     goodch = np.arange(31) # all plugged-in electrodes of the cap
     data         = data[goodch,:,:]
     # 3: apply spatial filter
-    spatialfilter='car'
     data         = preproc.spatialfilter(data,type=spatialfilter)
     # 4 & 5: spectral filter
-    freqbands    = [8, 10, 28, 30]
     data         = preproc.fftfilter(data, 1, freqbands, exp[n_exp]['hdr'].fSample)    
     # 6 : bad-trial removal
     goodtr, badtr = preproc.outlierdetection(data,dim=2)
@@ -84,9 +87,11 @@ good_errp   =   np.array(good)
 # Concatenate all the trials
 all_Data = np.concatenate((good_errp, bad_errp), axis=0)
 
+# constructing labels (0 -> wrong Errp, 1 -> correct Errp)
 y_g = np.ones((len(good_errp),),dtype=int)
 y_b = np.zeros((len(bad_errp),),dtype=int)
 y = np.concatenate((y_g, y_b), axis=0)
+
 
 ## Time features - classifiers
 
@@ -102,14 +107,10 @@ clsfr_time = sklearn.linear_model.RidgeCV(store_cv_values=True)
 
 clsfr_time.fit(scaler_time.transform(X_train_2d),y)
 
-cname = 'cazzo'
-norm_name = cname + '_Norm'
-treshold = 0.4431697721955611 # optimal threshold
-
-# save the trained classifer
+# save the trained classifer and normalizer
 print('Saving clsfr to : %s'%(cname+'.pk'))
-pickle.dump({'classifier':clsfr_time,'spatialfilter':spatialfilter,'freqbands':freqbands,'goodch':goodch,'treshold':treshold},open(cname+'.pk','wb'))
-pickle.dump({'normalizer':scaler_time},open(norm_name+'.pk','wb'))
+pickle.dump({'classifier':clsfr_time,'spatialfilter':spatialfilter,'freqbands':freqbands,'goodch':goodch,'treshold':treshold_time},open(cname_time+'.pk','wb'))
+pickle.dump({'normalizer':scaler_time},open(cname_time + '_Norm.pk','wb'))
 
 
 
@@ -124,7 +125,7 @@ f, psd = welch(all_Data,
             window='hanning',     # apply a Hanning window before taking the DFT
             nperseg= 125)   
 
-# select frequency bands of interests
+# select frequency bands of interests (from 8 to 30 Hz)
 f_8 = f > 7
 f_30 =f < 31
 f_8_30 = f_8 & f_30
@@ -143,12 +144,7 @@ clsfr_f = sklearn.linear_model.RidgeCV(store_cv_values=True)
 
 clsfr_f.fit(scaler_f.transform(X_train_2d),y)
 
-
-cname = 'pippo'
-norm_name = cname + '_Norm'
-treshold = 0.45619815088221327 # optimal threshold
-
 # save the trained classifer
 print('Saving clsfr to : %s'%(cname+'.pk'))
-pickle.dump({'classifier':clsfr_f,'spatialfilter':spatialfilter,'freqbands':freqbands,'goodch':goodch, 'treshold':treshold},open(cname+'.pk','wb'))
-pickle.dump({'normalizer':scaler_f},open(norm_name+'.pk','wb'))
+pickle.dump({'classifier':clsfr_f,'spatialfilter':spatialfilter,'freqbands':freqbands,'goodch':goodch, 'treshold':treshold_freq},open(cname_freq+'.pk','wb'))
+pickle.dump({'normalizer':scaler_f},open(cname_freq + '_Norm.pk','wb'))
